4d5c159335d614878133aaa5bb7c2ba92abf7505
Paul Rene Nichols <paul.rene.nichols@gmail.com>
2015-11-19 11:59:30 -0600

    added an instance of the DatePicker to the project App container

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
 
+import DatePicker from '../datepicker/index';
+
 
 export class App extends Component {
   render() {
     return (
-      <div>
-      </div>
+      <DatePicker visible={true} selectedDate={new Date(1129, 11, 0)}/>
     );
   }
 }
