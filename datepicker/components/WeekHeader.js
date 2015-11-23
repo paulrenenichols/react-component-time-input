0339f3b4660d90c7f5b1e1fe83eec9fd6a73d5fb
Paul Rene Nichols <paul.rene.nichols@gmail.com>
2015-11-23 13:26:16 -0600

    removed quotation marks from day names in WeekHeader

--- datepicker/components/WeekHeader.js
+++ datepicker/components/WeekHeader.js
@@ -7,13 +7,13 @@ class WeekHeader extends Component {
   render() {
     return (
       <div className={"week-header"}>
-        <span>"Sun"</span>
-        <span>"Mon"</span>
-        <span>"Tue"</span>
-        <span>"Wed"</span>
-        <span>"Thu"</span>
-        <span>"Fri"</span>
-        <span>"Sat"</span>
+        <span>Sun</span>
+        <span>Mon</span>
+        <span>Tue</span>
+        <span>Wed</span>
+        <span>Thu</span>
+        <span>Fri</span>
+        <span>Sat</span>
       </div>
     );
   }
