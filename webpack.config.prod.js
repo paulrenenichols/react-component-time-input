1ccdef47d5b29d4abd23a1a7b2e7ddf0f827f857
Paul Rene Nichols <paul.rene.nichols@gmail.com>
2015-11-24 11:47:51 -0600

    production webpack config now builds ReactDatePicker js and css in dist folder, exports ReactDatePicker object, and now treats React and ReactDOM as external and does not bundle them

--- webpack.config.prod.js
+++ webpack.config.prod.js
@@ -1,20 +1,28 @@
 var path = require('path');
 var webpack = require('webpack');
 
+var ExtractTextPlugin = require("extract-text-webpack-plugin");
+
 // node 0.10.xx does not support es6 promises.
 // This is a problem because the webpack style loaders need them.
 // This polyfill makes the webpack style loaders work in 0.10.xx versions of node.
 require('es6-promise').polyfill();
 
 module.exports = {
-  devtool: 'eval-source-map',
+  devtool: 'eval',
   entry: [
-    './src/index'
+    './datepicker/index'
   ],
   output: {
     path: path.join(__dirname, 'dist'),
-    filename: 'bundle.js',
-    publicPath: '/static/'
+    filename: 'ReactDatePicker.js',
+    publicPath: '/static/',
+    library: 'ReactDatePicker',
+    libraryTarget: 'commonjs'
+  },
+  externals: {
+    'react': 'React',
+    'react-dom': 'ReactDOM'
   },
   plugins: [
     new webpack.optimize.OccurenceOrderPlugin(),
@@ -27,17 +35,19 @@ module.exports = {
       compressor: {
         warnings: false
       }
-    })
+    }),
+    new ExtractTextPlugin("ReactDatePicker.css")
   ],
   module: {
     loaders: [{
       test: /\.js$/,
       loaders: ['babel'],
-      include: path.join(__dirname, 'src')
+      include: [path.join(__dirname, 'datepicker')],
+      exclude: [/node_modules/, path.join(__dirname, 'src')]
     },
     {
       test: /\.(css|less)$/,
-      loader: 'style!css!less'
+      loader: ExtractTextPlugin.extract("style-loader", "css-loader!less-loader")
     }]
   }
 };
