f96504ad934fa5ceb8fcfac22e2d147c23110f18
Paul Rene Nichols <paul.rene.nichols@gmail.com>
2015-11-24 09:39:40 -0600

    removed a lot of the old sample code from the Weeks component to simplify its structure and behavior

--- datepicker/components/Weeks.js
+++ datepicker/components/Weeks.js
@@ -1,71 +1,36 @@
 import React, { Component, PropTypes } from 'react';
 import ReactDOM from 'react-dom';
 
-import Week       from './Week';
+import Week from './Week';
 
 import DateUtilities from '../utility/DateUtilities';
 
 class Weeks extends Component {
 
-  state = {
-    view: new Date(this.props.view.getTime()),
-    other: new Date(this.props.view.getTime()),
-    sliding: null
+  static propTypes = {
+    displayDate: PropTypes.instanceOf(Date).isRequired,
+    selectedDate: PropTypes.instanceOf(Date).isRequired,
+    visible: PropTypes.bool.isRequired,
+    minimumDate: PropTypes.instanceOf(Date),
+    maximumDate: PropTypes.instanceOf(Date),
+    calendarMonth: PropTypes.array.isRequired
   }
 
-  componentDidMount() {
-    //ReactDOM.findDOMNode(this.refs.current).addEventListener("transitionend", this.onTransitionEnd);
-  }
-
-  getWeekStartDates(view) {
-    view.setDate(1);
-    view = DateUtilities.moveToDayOfWeek(new Date(view.getTime()), 0);
-
-    var current = new Date(view.getTime());
-    current.setDate(current.getDate()+7);
-
-    var starts = [view],
-        month = current.getMonth();
-
-    while (current.getMonth() === month) {
-      starts.push(new Date(current.getTime()));
-      current.setDate(current.getDate()+7);
-    }
-
-    return starts;
-  }
-
-  moveTo(view, isForward) {
-    this.setState({
-        sliding: isForward ? "left" : "right",
-        other: new Date(view.getTime())
-    });
+  constructor(props) {
+    super(props);
   }
 
   render() {
+    const { calendarMonth } = this.props;
     return (
       <div className={"weeks"}>
-        <div ref={"current"} className={"current" + (this.state.sliding ? (" sliding " + this.state.sliding) : "")}>
-          {this.renderWeeks(this.state.view)}
-        </div>
-
-        <div ref={"other"} className={"other" + (this.state.sliding ? (" sliding " + this.state.sliding) : "")}>
-          {this.renderWeeks(this.state.other)}
-        </div>
+        {calendarMonth.map((week, index) => {
+          return <Week key={index} week={week} {...this.props} />;
+        })}
       </div>
     );
   }
 
-  renderWeeks(view) {
-    var starts = this.getWeekStartDates(view),
-        month = starts[1].getMonth();
-
-    return (
-      starts.map((start, index) => {
-        return <Week key={index} start={start} month={month} selected={this.props.selected} onSelect={this.props.onSelect} minDate={this.props.minDate} maxDate={this.props.maxDate} />
-      })
-    );
-  }
 }
 
 export default Weeks;
