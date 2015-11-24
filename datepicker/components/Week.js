03c72560581527ba23549fd015e643132891026d
Paul Rene Nichols <paul.rene.nichols@gmail.com>
2015-11-24 09:41:16 -0600

    simplified the Week component by removing unnecessary code that came from the example

--- datepicker/components/Week.js
+++ datepicker/components/Week.js
@@ -1,61 +1,24 @@
 import React, { Component, PropTypes } from 'react';
 import ReactDOM from 'react-dom';
 
+import Day from './Day';
+
 import DateUtilities from '../utility/DateUtilities';
 
 class Week extends Component {
 
-  state = {
-  }
-
-  buildDays(start) {
-    var days = [new Date(start.getTime())],
-        clone = new Date(start.getTime());
-
-    for (var i = 1; i <= 6; i++) {
-        clone = new Date(clone.getTime());
-        clone.setDate(clone.getDate()+1);
-        days.push(clone);
-    }
-    return days;
-  }
-
-  isOtherMonth(day) {
-    return this.props.month !== day.month();
-  }
-
-  getDayClassName(day) {
-    var className = "day";
-    if (DateUtilities.isSameDay(day, new Date()))
-        className += " today";
-    if (this.props.month !== day.getMonth())
-        className += " other-month";
-    if (this.props.selected && DateUtilities.isSameDay(day, this.props.selected))
-        className += " selected";
-    if (this.isDisabled(day))
-        className += " disabled";
-    return className;
-  }
-
-  onSelect(day) {
-    if (!this.isDisabled(day)) {
-      this.props.onSelect(day);
-    }
-  }
-
-  isDisabled(day) {
-    var minDate = this.props.minDate,
-        maxDate = this.props.maxDate;
-
-    return (minDate && DateUtilities.isBefore(day, minDate)) || (maxDate && DateUtilities.isAfter(day, maxDate));
+  static propTypes = {
+    week: PropTypes.array.isRequired,
+    displayDate: PropTypes.instanceOf(Date).isRequired,
+    selectedDate: PropTypes.instanceOf(Date).isRequired
   }
 
   render() {
-    var days = this.buildDays(this.props.start);
+    const { week } = this.props;
     return (
-      <div className={"week"}>
-        {days.map((day, index) => {
-          return <div key={index} onClick={this.onSelect.bind(null, day)} className= {this.getDayClassName(day)}>{day.getDate()}</div>
+      <div className={"week"} {...this.props}>
+        {week.map((day, index) => {
+          return <Day key={index} day={day} {...this.props} />;
         })}
       </div>
     );
