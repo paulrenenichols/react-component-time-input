ed6df1bb02e8463469f7a0f36d4a0a1e56e3b995
Paul Rene Nichols <paul.rene.nichols@gmail.com>
2015-12-01 16:27:05 -0600

    TimeInput component is sloppy, but it kind of works.  committing the current state.

--- timeinput/components/TimeInput.js
+++ timeinput/components/TimeInput.js
@@ -19,15 +19,67 @@ class TimeInput extends Component {
     hours:   this.props.hours,
     minutes: this.props.minutes,
     seconds: this.props.seconds,
+    timeInputChangeTimeoutID: null,
+    error: false,
+    value: TimeUtilities.formatTimeTwelveHour({
+      hours: this.props.hours,
+      minutes: this.props.minutes,
+      seconds: this.props.seconds
+    })
   }
 
   constructor(props) {
     super(props);
   }
 
+  componentWillUpdate(nextProps, nextState) {
+    return true;
+  }
+
+  updateTimeInputValue = (timeString) => {
+    console.log('updateTimeInputValue(): this.state: ', this.state);
+    console.log('updateTimeInputValue(): timeString: ', timeString);
+    var timeObject = TimeUtilities.parseTime(timeString);
+    console.log('updateTimeInputValue(): timeObject: ', timeObject, ' !!timeObject ', !!timeObject);
+    if (timeObject) {
+      this.setState({
+        error: false,
+        timeInputChangeTimeoutID: null,
+        hours: timeObject.hours,
+        minutes: timeObject.minutes,
+        seconds: timeObject.seconds,
+        value: TimeUtilities.formatTimeTwelveHour({
+          hours: timeObject.hours,
+          minutes: timeObject.minutes,
+          seconds: timeObject.seconds
+        })
+      });
+    }
+    else {
+      console.log('updateTimeInputValue(): error: ', true);
+      this.setState({
+        error: true,
+        timeInputChangeTimeoutID: null
+      });
+    }
+  }
+
+  onTimeInputChange = (event) => {
+    if (this.state.timeInputChangeTimeoutID) {
+      clearTimeout(this.state.timeInputChangeTimeoutID);
+    }
+
+    var timeInputChangeTimeoutID = setTimeout(this.updateTimeInputValue, 1500, event.target.value);
+
+    this.setState({
+      timeInputChangeTimeoutID,
+      value: event.target.value
+    });
+  }
+
   render () {
     return (
-      <input type='text' value={TimeUtilities.formatTimeTwelveHour(this.state)}/>
+      <input style={ { backgroundColor: (this.state.error) ? 'red' : ''} } type='text' onChange={this.onTimeInputChange} value={this.state.value}/>
     );
   }
 }
