c21c7a5d742b00b476d33207658a5c1b3204c420
Paul Rene Nichols <paul.rene.nichols@gmail.com>
2015-12-01 14:32:28 -0600

    beginning to sketch out TimeInput now that TimeUtilities are in a working state.

--- timeinput/components/TimeInput.js
+++ timeinput/components/TimeInput.js
@@ -1,15 +1,24 @@
 import React, { Component, PropTypes } from 'react';
 import ReactDOM from 'react-dom';
 
+import TimeUtilities from '../utility/TimeUtilities';
+
 class TimeInput extends Component {
 
   static defaultProps = {
+    hours: 0
   }
 
   static propTypes = {
+    hours:   PropTypes.number.isRequired,
+    minutes: PropTypes.number,
+    seconds: PropTypes.number
   }
 
   state = {
+    hours:   this.props.hours,
+    minutes: this.props.minutes,
+    seconds: this.props.seconds,
   }
 
   constructor(props) {
@@ -18,7 +27,7 @@ class TimeInput extends Component {
 
   render () {
     return (
-      <input type='text' />
+      <input type='text' value={TimeUtilities.formatTimeTwelveHour(this.state)}/>
     );
   }
 }
