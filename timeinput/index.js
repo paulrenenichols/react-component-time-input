894b4b8a3801e2e0657d40e8b471e93b458cd0ec
Paul Rene Nichols <paul.rene.nichols@gmail.com>
2015-12-04 14:40:16 -0600

    alternative-components-0004
    * adding new listbox time input to the export object of the package's index.js

--- timeinput/index.js
+++ timeinput/index.js
@@ -1,12 +1,14 @@
 import React, { Component, PropTypes } from 'react';
 import ReactDOM from 'react-dom';
 
-import NumberInput   from './components/NumberInput';
-import TimeInputText from './components/TimeInputText';
-import TimeUtilities from './utility/TimeUtilities';
+import NumberInput      from './components/NumberInput';
+import TimeInputListBox from './components/TimeInputListBox';
+import TimeInputText    from './components/TimeInputText';
+import TimeUtilities    from './utility/TimeUtilities';
 
 const ReactTimeInput = {
   NumberInput,
+  TimeInputListBox,
   TimeInputText,
   TimeUtilities
 }
